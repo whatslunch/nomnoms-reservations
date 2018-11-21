8923ec8bb670ca610d5db72babdf6d8602feb168
Boyun Zou <zouboyun@Boyuns-MacBook-Pro.local>
2018-11-20 23:21:29 -0800

    updated cassandra db schema and seeding files

---
+++ server/db1/seed.js
@@ -0,0 +1,28 @@
+/* eslint-disable no-console */
+const path = require('path');
+const stream = require('fs').createReadStream(path.join(__dirname, '../../data/restaurantsNoSQL.csv'));
+const readline = require('readline').createInterface({
+  input: stream,
+});
+const connection = require('./connection');
+
+let lineCounter = 0;
+
+readline.on('line', (line) => {
+  if (line) {
+    readline.pause();
+    const query = 'INSERT INTO reservations.restaurants JSON ? ;';
+    connection.execute(query, [line], { prepare: true }, (error) => {
+      if (error) {
+        throw error;
+      } else {
+        lineCounter += 1;
+        if (lineCounter === 10000000) {
+          readline.close();
+          connection.shutdown();
+        }
+        readline.resume();
+      }
+    });
+  }
+});
