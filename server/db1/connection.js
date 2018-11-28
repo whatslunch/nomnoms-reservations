985df4d12cd6c5ce49c7c4a47d9c2e026417d2ce
Boyun Zou <zouboyun@Boyuns-MacBook-Pro.local>
2018-11-28 14:41:04 -0800

    updated seeding script

--- server/db1/connection.js
+++ server/db1/connection.js
@@ -3,7 +3,11 @@
 const cassandra = require('cassandra-driver');
 
 // change the contactPoints to your own end point
-const client = new cassandra.Client({ contactPoints: ['127.0.0.1'] });
+const authProvider = new cassandra.auth.PlainTextAuthProvider('cassandra', 'cassandra');
+const client = new cassandra.Client({
+  contactPoints: ['127.0.0.1'],
+  authProvider,
+});
 
 client.connect((err) => {
   if (err) {
